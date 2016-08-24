47b150dafb88367f6c081efddfcc44f59daaceb2
christianeggbauer <christianeggbauer@christians-mbp>
2016-08-24 11:42:02 +0200

    My first commit ;)

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println((1+1+1)+" = 3");
+        int i;
+        i = 5;
+        if (i == 3) { System.out.println(i); } else System.out.println("i ist != 3, i = "+i);
+    }
+}
