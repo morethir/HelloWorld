bb22a1ea65ad0077aef11fcef3e19d88ffb07e4c
christianeggbauer <christianeggbauer@christians-mbp>
2016-08-24 12:21:34 +0200

    2. Push

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,8 +3,16 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println((1+1+1)+" = 3");
-        int i;
-        i = 5;
+        int i = 5;
         if (i == 3) { System.out.println(i); } else System.out.println("i ist != 3, i = "+i);
+        String j;
+        j = "First Test successfully!";
+        System.out.println(j);
+        SuperDupa();
+        
     }
+    private static void SuperDupa() {
+    	System.out.println("Hiho");
+    }
+    
 }
