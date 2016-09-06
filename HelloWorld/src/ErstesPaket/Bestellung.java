9936e15b987cfb0342b901d172a2afd4527b1ce9
christianeggbauer <christianeggbauer@christians-mbp>
2016-09-06 13:26:09 +0200

    Abfrage Gaming PC if-else Anweisung

--- HelloWorld/src/ErstesPaket/Bestellung.java
+++ HelloWorld/src/ErstesPaket/Bestellung.java
@@ -2,16 +2,17 @@ package ErstesPaket;
 
 public class Bestellung {
 	public static void main(String[] args) {
-		Computer gamingPC = new Computer("Gaming Hot", "4GHz", "Radeon 9800", 16, 1400);
+		Computer gamingPC = new Computer("Gaming Hot", "4GHz", "Radeon 9800", 16, 400);
 		Computer Notebook = new Computer("Mobile Best", "2GHz", "Geforce M", 8, 1299);
 		Computer OfficePC = new Computer("Office PC", "1.8 GhZ", "Intel 550", 4, 449);
 		
-		gamingPC.datenblatt();
-		Notebook.datenblatt();
-		OfficePC.datenblatt();
+		// gamingPC.datenblatt();
+		// Notebook.datenblatt();
+		// OfficePC.datenblatt();
+		if (gamingPC.preis > 1000) {
+			gamingPC.datenblatt(); 
+			} else System.out.println("Kein Gaming PC über 1000 EUR vorhanden!");
 		
-		gamingPC.prozessor = "8GHz";
-		gamingPC.datenblatt();
 		
 	}
 }
